688b381c0bc9d486c1973f8eef6a1bc7314a4dfd
Md Sha Newaz <sawonnstu@gmail.com>
2023-09-04 01:19:21 +0600

    last

--- first_app/forms.py
+++ first_app/forms.py
@@ -2,6 +2,5 @@ from django import forms
 from first_app import models
 
 class user_form(forms.Form):
-    user_name = forms.CharField()
-    user_email = forms.EmailField()
-    
+    user_name = forms.CharField(required=False, label='Name', initial='Sawon')
+    user_email = forms.EmailField(label='Email')
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Musician, Album
+from first_app import forms
 
 # Create your views here.
 
@@ -13,7 +14,11 @@ def home(request):
     return render(request, 'first_app/index.html', context=diction)
 
 def form(request):
-    diction = {}
+    new_form = forms.user_form()
+    diction = {
+        'test_form' : new_form,
+        'heading_1' : 'This form is created using django Library',
+    }
     return render(request, 'first_app/form.html', context=diction)
 
 def contact(request):

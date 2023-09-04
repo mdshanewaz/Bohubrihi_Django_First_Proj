261d2f0125fbeaeab0ba8560be3c2d71c8c15f56
Md Sha Newaz <sawonnstu@gmail.com>
2023-09-05 00:20:42 +0600

    New

--- first_app/forms.py
+++ first_app/forms.py
@@ -3,4 +3,12 @@ from first_app import models
 
 class user_form(forms.Form):
     user_name = forms.CharField(required=False, label='Name', initial='Sawon')
-    user_email = forms.EmailField(label='Email')
+
+    user_email = forms.EmailField(label='Email', widget= forms.TextInput(attrs={
+        'placeholder' : 'Enter a valid email',
+        'style' : 'width: 300px',
+    }))
+
+    user_dob = forms.DateField(label="Date of Birth", widget = forms.TextInput(attrs={
+        'type' : 'date',
+    }))
--- first_app/views.py
+++ first_app/views.py
@@ -19,6 +19,21 @@ def form(request):
         'test_form' : new_form,
         'heading_1' : 'This form is created using django Library',
     }
+
+    if request.method == 'POST':
+        
+        new_form = forms.user_form(request.POST)
+        
+        if new_form.is_valid():
+            user_name = new_form.cleaned_data['user_name']
+            user_email = new_form.cleaned_data['user_email']
+            user_dob = new_form.cleaned_data['user_dob']
+
+            diction.update({'user_name' : user_name})
+            diction.update({'user_email' : user_email})
+            diction.update({'user_dob' : user_dob})
+            diction.update({'form_submited' : "Yes"})
+
     return render(request, 'first_app/form.html', context=diction)
 
 def contact(request):

7c236700e640eab964d36972c91502a7417d7ff6
Md Sha Newaz <sawonnstu@gmail.com>
2023-09-08 16:04:53 +0600

    New

--- first_app/urls.py
+++ first_app/urls.py
@@ -4,8 +4,8 @@ from first_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add_album/', views.album_form, name="album_form"),
-    path('add_musician/', views.musician_form, name="musician_form"),
+    path('add_album/', views.album_form, name='album_form'),
+    path('add_musician/', views.musician_form, name='musician_form'),
     path('album_list/<int:artist_id>/', views.album_list, name='album_list'),
     path('form/', views.form, name='form'),
 ]

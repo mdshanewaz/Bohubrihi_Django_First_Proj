67a627e3eb408c5bf11189c8bb1f3034cdbdf1af
Md Sha Newaz <sawonnstu@gmail.com>
2023-09-16 00:00:44 +0600

    new

--- first_app/views.py
+++ first_app/views.py
@@ -124,6 +124,46 @@ def edit_artist(request, artist_id):
             return album_list(request, artist_id)
 
     diction = {
-        'edit_form' : form
+        'edit_form' : form,
+        'title' : "Edit Musician",
     }
     return render(request, 'first_app/edit_artist.html', context=diction)
+
+def edit_album(request, album_id):
+    album_info = Album.objects.get(pk=album_id)
+    form = forms.AlbumForm(instance=album_info)
+    diction = {}
+
+    if request.method == 'POST':
+        form = forms.AlbumForm(request.POST, instance=album_info)
+
+        if form.is_valid():
+            form.save(commit=True)
+            diction.update({'success_text':'Successfully Updated'})
+
+    diction.update({
+        'edit_form' : form,
+        'title' : "Edit Album",
+        'album_id' : album_id
+    })
+
+    return render(request, 'first_app/edit_album.html', context=diction)
+
+def delete_album(request, album_id):
+    album_info = Album.objects.get(pk=album_id).delete()
+    
+    diction = {
+        'title' : "Delete Album",
+        'delete_success' : 'Album is deleted'
+           }
+    return render(request, 'first_app/delete.html', context=diction)
+
+def delete_artist(request, artist_id):
+    artist_info = Musician.objects.get(pk=artist_id).delete()
+
+    diction = {
+        'title' : "Delete Musician",
+        'delete_success' : 'Musician is deleted'
+    }
+
+    return render(request, 'first_app/delete.html', context=diction)
